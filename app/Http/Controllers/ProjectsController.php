baef5a690404a5c02d5abcbbda767abce4a7c20a
mmghunaim <mghunaim0311@gmail.com>
2020-01-17 22:24:25 +0200

    More TDDs and Some Refactoring

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -8,21 +8,41 @@ class ProjectsController extends Controller
 {
     public function index()
     {
-        $projects= Project::all();
+        // $projects= Project::all();
+        $projects = auth()->user()->projects;
 
         return view('projects.index', ['projects'=> $projects]);
     }
 
+    public function show(Project $project)
+    {
+        // if (auth()->user()->isNot($project->owner)) {
+        //     abort(403);
+        // }
+
+        abort_if(auth()->user()->isNot($project->owner), 403);
+
+        return view('projects.show',['project'=> $project]);
+    }
+
     public function store()
     {
-        $attributes= request()->validate(['title'=>'required', 'description'=> 'required']);
+        $attributes= request()->validate(
+            [
+                'title'=>'required',
+                'description'=> 'required'
+            ]);
 
-        Project::create($attributes);
+        // $attributes['owner_id'] = auth()->id();
+
+        auth()->user()->projects()->create($attributes);
+
         return redirect('/projects');
     }
 
-    public function show(Project $project)
+    public function create()
     {
-        return view('projects.show',['project'=> $project]);
+        return view('projects.create');
     }
+
 }
--- app/Project.php
+++ app/Project.php
@@ -6,10 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    protected $fillable = ['title','description','owner_id'];
     protected $guarded=[];
 
     public function path()
     {
         return "/projects/{$this->id}";
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
     }
 }
