bc2927494706fa6116cd1601aabc3b68ed803c3c
mmghunaim <mghunaim0311@gmail.com>
2020-01-18 15:18:31 +0200

    Create custom Tailwind component's button

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,16 +2,26 @@
 
 @section('content')
 <body>
-    <div class="flex items-center mb-3">
+    <header class="flex items-center mb-3 py-4 px-1 ">
         {{-- <h1 class="mr-auto">BirdBoard</h1> --}}
-        <a href="/projects/create">Create New Project</a>
-    </div>
+        <div class="flex justify-between w-full items-center">
+            <div class="text-gray-600">
+                My Projects
+            </div>
 
-    <div class="flex flex-wrap -mx-3">
+            <div>
+                <a href="/projects/create" class="button">Create New Project</a>
+            </div>
+        </div>
+    </header>
+
+    <main class="lg:flex flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded shadow" style="height: 200px">
-                    <h2 class="py-4">{{ $project->title }}</h2>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+                    <h2 class="py-4 -ml-5 border-l-4 border-blue-200 pl-4 mb-3">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h2>
 
                     <div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>
                 </div>
@@ -20,7 +30,7 @@
             <div>No Projcets Yet.</div>
         @endforelse
                 
-    </div>
+    </main>
 
 </body>
 </html>
