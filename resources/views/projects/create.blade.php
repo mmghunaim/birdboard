0c94f9bf9bd9ffc98660ba060a37def59f75da46
mmghunaim <mghunaim0311@gmail.com>
2020-01-20 00:07:05 +0200

    changes on create project page

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,33 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<body>
-    <form method="POST" action="/projects">
+
+    <form 
+    method="POST"
+    action="/projects"
+    class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow" 
+    >
         @csrf
-        <h1 class="heading is-1">Create a Project</h1>
-        <div class="field">
-            <label class="lable" for="title">Title</label>
-        
+
+        <h1 class="text-2xl font-normal mb-10 text-center">
+        Let's start something new
+        </h1>
+
+        <div class="field mb-6">
+            <label class="lable text-sm mb-2 block" for="title">Title</label>     
             <div class="control">
-                <input type="text" class="input" name="title" placeholder="Title">
+                <input 
+                type="text" 
+                class="input bg-transparent border border-grey-600 rounded p-2 text-xs w-full" 
+                name="title" 
+                placeholder="Hmmmm"
+                >
             </div>
         </div>
 
         <div class="field">
-            <label class="lable" for="description">Description</label>
-                
+            <label class="lable text-sm mb-2 block" for="description">Description</label>    
             <div class="control">
-                <textarea class="textarea" name="description"></textarea>
+                <textarea 
+                class="input bg-transparent border border-grey-600 rounded p-2 text-xs w-full h-40" 
+                 name="description" 
+                 placeholder="Feel free to type your description here.">
+                 </textarea>
             </div>
         </div>
 
         <div class="field">
             <div class="control">
-                <button type="submit" class="button is-link">Create a Projet</button>
-                <a href="/projects">Cancel</a>
+                <button 
+                type="submit" 
+                class="button is-link mr-2">
+                Create Project
+                </button>
+                <a href="/projects" class="text-default">Cancel</a>
             </div>
         </div>
 
     </form>
-</body>
 @endsection   
