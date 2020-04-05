0c2391181a1374e240829f14897948090a6ddc9d
Mohammed Ghunaim <mghunaim0311@gmail.com>
2020-04-05 20:03:36 +0000

    Apply fixes from StyleCI

--- app/Events/ProjectPublished.php
+++ app/Events/ProjectPublished.php
@@ -2,11 +2,8 @@
 
 namespace App\Events;
 
-use App\Project;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -16,6 +13,7 @@ class ProjectPublished implements ShouldBroadcast
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $project;
+
     /**
      * Create a new event instance.
      *
